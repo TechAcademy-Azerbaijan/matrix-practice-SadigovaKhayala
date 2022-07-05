b1965a635bd0a430808bcd477acdd526cd40da91
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-05 20:20:41 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,8 +1,17 @@
-// Use this matrix for solution
 let matrix = [
     [4, -2, 5],
     [1, -4, -12],
     [0, 1, -3]
 ]
+let count=0;
+let sum=0;
 
-// Write code here. 
+for(i=0;i<matrix.length;i++){
+    for(j=0;j<matrix.length;j++)
+    if(matrix[i][j]%2==0&&matrix[i][j]<0){
+        sum=sum+matrix[i][j];
+        count++
+    }
+}
+
+console.log(sum)
